d3bcadf7c19d3033a7a62e9e0d7114b6b194c7b9
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2023-08-13 00:37:01 +0800

    修复第三方无法抓取的问题

--- custom_components/ha_cloud_music/music_parser.py
+++ custom_components/ha_cloud_music/music_parser.py
@@ -9,26 +9,27 @@ def get_music(keyword):
   try:
     response = session.get(f'{api}/s/{keyword}')
     soup = BeautifulSoup(response.text.encode(response.encoding), 'lxml')
-    items = soup.select('.table tbody tr')
-    if len(items) > 0:
-      td = items[0].select('td')
+    items = soup.select('.card-text .row')
+    if len(items) > 1:
+      row = items[1]
       # print(td)
-      song = td[0].get_text().strip()
-      singer = td[1].get_text().strip()
+      song = row.select('.col-5 a')[0].get_text().strip()
+      singer = row.select('.col-4')[0].get_text().strip()
 
-      a = items[0].select('a')
+      a = row.select('.col-3 a')
       href = a[0].attrs['href']
       
       # print(href)
       response = session.get(f'{api}{href}')
       html = response.text
+
       pattren = re.compile(r'https://[^\s]+.mp3')
       url_lst = pattren.findall(html)
       # print(url_lst)
       if len(url_lst) > 0:
         soup = BeautifulSoup(html, 'lxml')
         cover = soup.select('head meta[property="og:image"]')
-        print(cover)
+        #print(cover)
         pic = 'https://p2.music.126.net/tGHU62DTszbFQ37W9qPHcg==/2002210674180197.jpg'
         # 封面
         if len(cover) > 0:
